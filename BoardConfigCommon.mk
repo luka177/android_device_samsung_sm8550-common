466ea3d7a5f5364d3021e3d5695efc253815c065
J0SH1X <aljoshua.hell@gmail.com>
2024-01-06 10:10:04 +0100

    Build kernel from source

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -83,16 +83,16 @@ BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000
 
 BOARD_BOOTCONFIG += androidboot.selinux=permissive
-
-BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_SEPARATED_DTBO := true
+BOARD_RAMDISK_USE_LZ4 := true
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
-
-TARGET_FORCE_PREBUILT_KERNEL := true
 TARGET_KERNEL_SOURCE := kernel/samsung/sm8550
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/$(HOST_OS)-x86/clang r450784e
 TARGET_KERNEL_CONFIG := \
-    gki_defconfig \
-    vendor/kalama_GKI.config
+    gki_defconfig
+KERNEL_LTO := none
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
