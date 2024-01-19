09fa4e0923bc701e5d8aa9c6ecb2a32f3561e98b
Luka Panio <lukapanio@gmail.com>
2024-01-19 19:01:26 +0100

    sm8550-common: Build libaudiochargerlistener
    Change-Id: Ie0aea86280c4f2eda94d06e93147eeaf8d022ad3

--- common.mk
+++ common.mk
@@ -36,6 +36,7 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
+    libaudiochargerlistener \
     libbatterylistener \
     libqcompostprocbundle \
     libqcomvisualizer \
