a9ec9c4e9d59a4030a4ccadc2bf7e5a0312f745e
Luka Panio <lukapanio@gmail.com>
2024-01-26 09:49:53 +0100

    sm8550-common: Build android.hardware.bluetooth.audio-impl from sources
    Change-Id: I2159050be9a5e570c1e72a39826046dc0995bb60

--- common.mk
+++ common.mk
@@ -26,6 +26,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
     audio.bluetooth.default \
     audio.primary.kalama \
