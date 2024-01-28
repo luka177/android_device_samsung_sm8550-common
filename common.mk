c8c027bc8e2d9050f6886ee678560a449b232e38
Luka Panio <lukapanio@gmail.com>
2024-01-28 13:44:09 +0100

    sm8550-common: Build gatekeeper from sources
    Change-Id: I1848a72a536e35f388b3eccf690ea673e13307a5

--- common.mk
+++ common.mk
@@ -161,7 +161,8 @@ PRODUCT_PACKAGES += \
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
-    android.hardware.gatekeeper@1.0.vendor
+    android.hardware.gatekeeper@1.0-impl \
+    android.hardware.gatekeeper@1.0-service
 
 # Graphics
 PRODUCT_COPY_FILES += \
